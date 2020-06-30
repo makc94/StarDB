966428485f6abf40c2de3719dfd4af6a3287c062
Your Name <you@example.com>
2020-06-30 21:07:54 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+class SwapiService {
+
+  async getResource() {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}` + ` , received ${res.status}`);
+    }
+
+    return await res.json();
+  }
+}
